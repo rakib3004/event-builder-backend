9e877fb5667fcfb9e78ae3f6e9b4c34608d924ba
rakib3004 <bsse1129@iit.du.ac.bd>
2025-05-27 11:08:03 +0600

    change configuration for local

--- app/core/config.py
+++ app/core/config.py
@@ -8,21 +8,21 @@ class Settings(BaseSettings):
 
     # PostgreSQL Configuration
     POSTGRES_USER: str = os.getenv("POSTGRES_USER", "postgres")
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "112233")
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
-    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", "5432")
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "simple_event_builder")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "P0StGr35**")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "202.4.127.189")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", "5632")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "event_builder")
     DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     # MinIO Configuration
     MINIO_ENDPOINT: str = os.getenv("MINIO_ENDPOINT", "localhost:9100")
     MINIO_ACCESS_KEY: str = os.getenv("MINIO_ACCESS_KEY", "")
     MINIO_SECRET_KEY: str = os.getenv("MINIO_SECRET_KEY", "")
-    MINIO_BUCKET_NAME: str = os.getenv("MINIO_BUCKET_NAME", "event-images")
+    MINIO_BUCKET_NAME: str = os.getenv("MINIO_BUCKET_NAME", "replica-event-images")
     MINIO_SECURE: bool = os.getenv("MINIO_SECURE", "False").lower() == "true"
 
     # CORS
-    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:7010"] # Angular dev server
+    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:4200"] # Angular dev server
 
     class Config:
         case_sensitive = True
